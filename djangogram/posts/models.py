0749379b008a66fde9e8d8364178b06d8aedad49
Yoning Lee <0517lyi@naver.com>
2021-08-24 16:34:12 +0900

    feat(password): provide password in hashed format with widget

--- djangogram/posts/models.py
+++ djangogram/posts/models.py
@@ -4,10 +4,8 @@ from djangogram.users import models as user_model
 class TimeStamedModel(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-
-    #This model will then not be used to create any database table. 
-    # Instead, when it is used as a base class for other models, 
-    # its fields will be added to those of the child class.
+    
+    #추상적인 모델임을 선언, 상속만 가능
     class Meta:
         abstract = True
 
