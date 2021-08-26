21d318476d7e9ec8a9324d2ec6737201c4f5e658
Yoning Lee <0517lyi@naver.com>
2021-08-26 23:45:03 +0900

    Feat(post): Create  posts_create function and urls

--- djangogram/posts/urls.py
+++ djangogram/posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create/', views.posts_create, name='posts_create')
 ]
--- djangogram/posts/views.py
+++ djangogram/posts/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'posts/base.html')
+
+def posts_create(request):
+    if request.method == 'GET':
+        return render(request, 'posts/posts_create.html')
+    elif request.method == 'POST':
+        pass
