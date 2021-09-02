608d543209591eac0866125016f9b58a81460744
Yoning Lee <0517lyi@naver.com>
2021-09-02 22:35:24 +0900

    Feat(post): Create post_create.html using forms.py and Render it in views.py

--- djangogram/posts/views.py
+++ djangogram/posts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from djangogram.users.models import User as user_model
+
 from django.db import models
+from .forms import CreatePostForm
+
 # Create your views here.
 def index(request):
     return render(request, 'posts/base.html')
 
 def posts_create(request):
     if request.method == 'GET':
-        return render(request, 'posts/posts_create.html')
+        form = CreatePostForm()
+        return render(request, 'posts/posts_create.html', {"form":form})
 
     elif request.method == 'POST':
         if request.user.is_authenticated:
