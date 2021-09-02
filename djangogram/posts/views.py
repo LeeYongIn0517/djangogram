9e2e4cbc6b029fd5c404e61c7c4baf693e6d453d
Yoning Lee <0517lyi@naver.com>
2021-09-02 23:17:08 +0900

    Feat(post): Create post.author using CreatePostForm and Register post page and next page

--- djangogram/posts/views.py
+++ djangogram/posts/views.py
@@ -17,14 +17,24 @@ def posts_create(request):
         if request.user.is_authenticated:
             user = get_object_or_404(user_model, pk=request.user.id)
             
-            image = request.FilES['image']
-            caption = request.POST['caption']
+            # image = request.FilES['image']
+            # caption = request.POST['caption']
 
-            new_post = models.Post.objects.create(
-                author = user,
-                image = image,
-                caption = caption
-            )
-            new_post.save()
+            # new_post = models.Post.objects.create(
+            #     author = user,
+            #     image = image,
+            #     caption = caption
+            # )
+            # new_post.save()
+             
+            form = CreatePostForm(request.POST, request.FILES)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = user
+                post.save()
+            else:
+                print(form.errors)
+                
+            return render(request, 'posts/main.html')
         else:
             return render(request, 'users/main.html')
