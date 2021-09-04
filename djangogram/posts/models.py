dc41bc2c04436db5deabfeea881a806bf45870b2
Yoning Lee <0517lyi@naver.com>
2021-09-04 16:16:22 +0900

    no message

--- djangogram/posts/models.py
+++ djangogram/posts/models.py
@@ -4,7 +4,7 @@ from djangogram.users import models as user_model
 class TimeStamedModel(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    
+
     #추상적인 모델임을 선언, 상속만 가능
     class Meta:
         abstract = True
@@ -14,7 +14,7 @@ class Post(TimeStamedModel):
             user_model.User, #외래키
             null=True, #필드 값이 NULL로 저장되는 것 허용
             on_delete=models.CASCADE, #계정 삭제시 해당 정보도 삭제
-            related_name='post_author' #특정 사용자가 작성한 질문을 얻을 때 사용, ex)some_user.post_author.all()                                       
+            related_name='post_author' #특정 사용자가 작성한 질문을 얻을 때 사용, ex)some_user.post_author.all()
             )
     image = models.ImageField(blank=False)
     caption = models.TextField(blank=False)
@@ -23,24 +23,24 @@ class Post(TimeStamedModel):
             blank=True,
             related_name='post_images_likes'
         )
-    
+
     def __str__(self):
-        return f"{self.author}: {self.caption}" 
+        return f"{self.author}: {self.caption}"
 
 class Comment(TimeStamedModel):
     author = models.ForeignKey(
             user_model.User,
             null=True,
             on_delete=models.CASCADE, #계정 삭제시 해당 정보도 삭제
-            related_name='comment_author' #특정 사용자가 작성한 질문을 얻을 때 사용, ex)some_user.post_author.all()                                       
+            related_name='comment_author' #특정 사용자가 작성한 질문을 얻을 때 사용, ex)some_user.post_author.all()
             )
     posts = models.ForeignKey(
             Post,
             null=True,
             on_delete=models.CASCADE, #계정 삭제시 해당 정보도 삭제
-            related_name='comment_post' #특정 사용자가 작성한 질문을 얻을 때 사용, ex)some_user.post_author.all()                                       
+            related_name='comment_post' #특정 사용자가 작성한 질문을 얻을 때 사용, ex)some_user.post_author.all()
             )
     contents = models.TextField(blank=True)
 
     def __str__(self):
-        return f"{self.author}: {self.contents}" 
+        return f"{self.author}: {self.contents}"
